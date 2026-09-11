40dcb1c6a399c06fa991d1a9354afbcacdc119aa
jcaro0 <jcarocarrera@gmail.com>
2026-09-11 09:14:26 +0200

    Haciendo mas pruebas con kivy + documentacion instalada

--- probando_kivy.py
+++ probando_kivy.py
@@ -6,13 +6,15 @@ from kivy.uix.textinput import TextInput
 class LoginScreen(GridLayout):
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
+        self.rows = 2
         self.add_widget(Label(text='User Name'))
         self.username = TextInput(multiline=False)
         self.add_widget(self.username)
         self.add_widget(Label(text='password'))
         self.password = TextInput(password=True, multiline=False)
         self.add_widget(self.password)
+        # self.add_widget(Label(text=f"La contraseña de {self.username} es: {self.password}"))
+        # self.add_widget(Label(text='La contraseña de tal es pascual'))
 
 class MyApp(App):
     def build(self):
